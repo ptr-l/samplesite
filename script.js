bd9ecc918b9af38fb29169b2a19413298671c76d
Peter Lamson <ptrlmsn@gmail.com>
2021-10-22 16:32:49 -0400

    Added Buttons + Popout Descriptions.

---
+++ script.js
@@ -0,0 +1,40 @@
+let Sam = document.getElementById('Sam')
+Sam.addEventListener("click", function () {
+    alert("An easy to learn, quick playing game of set collection and hidden information - players compete to control the three differenty types of resources that dot the islands of Japan.");
+      });
+let Obs = document.getElementById('Obs')
+      Obs.addEventListener("click", function () {
+          alert("Pride & Prejudice, the Board Game - players compete in a tableau building game to rebuild their family's fortunes, and attract the attention of Derbyshire's most eligible heirs.");
+            });
+let Antq = document.getElementById('Antq')
+Antq.addEventListener("click", function () {
+    alert("Don't let the muted looks of this game fool you - it is a deep, tactical experience, as the players take the role of medieval Italian governors trying to manage their city's resources, polution, and neighbors while aiming for asymmetric victory conditions.");
+});
+let NR = document.getElementById('NR')
+NR.addEventListener("click", function () {
+    alert("A two-games in one entry in Carl Paradis' No Retreat series of WWII war games. Players will take control of either side of the German invasion of Poland or France, as they manage their forces and a hand of cards to try and change history.")
+});
+let DS = document.getElementById('DS')
+DS.addEventListener("click", function () {
+    alert("This game's drab looks famously inspired 'The Cones of Dunshire' - but it remains an all-time classic from the late Chad Jensen. Players take the role of one of a number of classes of animals, as they compete to control territory hexes and outrace the ever expanding ice. Once the meteor falls and spells the end of an era, the Dominant Species is crowned.")
+});
+let Lis = document.getElementById('Lis') 
+Lis.addEventListener("click", function () {
+    alert("With beautiful production by Ian O'Toole, Lacerda's masterpiece about the reconstruction of Lisbon after the earthquake of 1755 lives up to the designer's reputation for heavyweight games. Players will have to balance multiple paths to victory and a variety of interconnected systems, as they seek favor with the royal court, influence the Cardinals, clean up after the disaster, and reconstruct the city.")
+})      ;
+let NT = document.getElementById('NT')
+NT.addEventListener("click", function () {
+    alert("With the decision to focus on the core of movement, timing, and command rather than bogging the action down with a series of hyper-detailed tables, rules, and addendums Simmons created a high-water mark for wargaming. Refight Napoleon's crushing victory at Austerlitz in three hours, and see where history could have diverged.")
+});
+let Trn = document.getElementById('Trn')
+Trn.addEventListener("click", function (){
+    alert("Taking the bones of the formative deckbuilder and adding a board, Hayashi created a quick playing and light routebuilding game. Purchase more and better trains, expand your deck, but wathc out for the waste cards that threaten to clog up your hand!")
+});
+let sgp = document.getElementById('SGP')
+sgp.addEventListener("click", function (){
+    alert("A card drafting game where players work to select the best combination of cute, cartoonified sushi dishes to end up with the best menu after three rounds of scoring. Light, fast playing, and with a large variety of possible setups - an easy reccomend to anyone looking for games for a larger group.")
+});
+let TV = document.getElementById('TV')
+TV.addEventListener("click", function () {
+    alert("Weighing in at the border of medium and heavy, The Vote is a game where one player competes to expand equal voting rights, while the other player represents the traditonalist forces fighting against the suffragists. The result is a game loaded with history and tactical nuance")
+});
